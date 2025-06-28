78659032e9fa06f7bc21e4f06be74d51ddb1aeb2
Tovix <mostafamohamed123345@yahoo.com>
2025-06-29 00:55:04 +0300

    performed cleaning on the 'discharge_disposition_id' column

--- analysis.py
+++ analysis.py
@@ -69,7 +69,50 @@ if __name__ == "__main__":
       
       df.drop(columns=medication_columns, inplace=True)
       print(df.info())
-      
+      # Now we move on to the analysis of the discharge_disposition_id column
+      print(sorted(list(df['discharge_disposition_id'].unique())))
+      # so we knew those values ranging from 1 to 29 representing the degree of the disposition 
+      # so we generated a list with values matching from 1 to 29 and we are going to do the same thing
+      # we did with adimission type
+      discharge_disposition_ranked = {
+    29: "29-Home(RoutineDischarge)",
+    28: "28-HomeWithHomeHealthServices",
+    27: "27-HomeWithSelfCareAssistance",
+    26: "26-HomeHospice",
+    25: "25-OutpatientRehabilitation",
+    24: "24-PhysiciansOfficeFollowUp",
+    23: "23-SkilledNursingFacility(ShortTerm)",
+    22: "22-AssistedLivingFacility",
+    21: "21-PsychiatricFacility(Voluntary)",
+    20: "20-SubstanceAbuseTreatment",
+    19: "19-LongTermAcuteCareHospital",
+    18: "18-InpatientRehabilitationFacility",
+    17: "17-SkilledNursingFacility(LongTerm)",
+    16: "16-HospiceFacility",
+    15: "15-PsychiatricFacility(Involuntary)",
+    14: "14-FederalHealthcareFacility",
+    13: "13-PrisonJail",
+    12: "12-GroupHome",
+    11: "11-BoardAndCareHome",
+    10: "10-IntermediateCareFacility",
+    9: "9-LeftAgainstMedicalAdvice",
+    8: "8-Expired(DiedInHospital)",
+    7: "7-HomelessShelter",
+    6: "6-UnknownNoFixedAddress",
+    5: "5-AcuteCareHospitalTransfer",
+    4: "4-StillPatient(DischargePending)",
+    3: "3-Morgue",
+    2: "2-ForensicCustodialFacility",
+    1: "1-Nowhere(AbandonedUnclaimed)"
+}
+      # now, we will check the diag_1 in coresponse with discharged_position
+      print(df[df['admission_type_id'] == 'resuscitation'][['diag_1', 'discharge_disposition_id']].head(20))
+      df['discharge_disposition_id'] = df['discharge_disposition_id'].map(discharge_disposition_ranked).astype('category')
+      print(df.info())
+      # now we check the admission_source_id:
+      print(sorted(list(df['admission_source_id'].unique())))
+
+
 
 
 
