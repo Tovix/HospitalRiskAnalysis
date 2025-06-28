fcc545634a5f1f960d4cf849b2764643324e1440
Tovix <mostafamohamed123345@yahoo.com>
2025-06-28 22:40:51 +0300

    init the project with current progress

---
+++ analysis.py
@@ -0,0 +1,84 @@
+import ucimlrepo
+import numpy as np
+import pandas as pd
+
+# fetch data for the first runtime
+def fetchDataset() -> ucimlrepo.dotdict:
+      # fetch dataset 
+      diabetes_130_us_hospitals_for_years_1999_2008 = ucimlrepo.fetch_ucirepo(id=296)
+      return diabetes_130_us_hospitals_for_years_1999_2008
+
+# analyizing total null values in the dataset
+def nullChecker(df: pd.DataFrame) -> pd.Series:
+      return df.isna().sum()
+
+
+if __name__ == "__main__":
+      df = pd.read_csv('data/diabetic_data.csv')
+      print(df.head())
+      print(nullChecker(df))
+      # from the null checking we can conclude that the two main cols with missing vals are
+      # A1Cresult, max_glu_serum, we shall exmaine them seperately.
+      print(df[['max_glu_serum', 'A1Cresult']].value_counts())
+      print(df['max_glu_serum'].unique())
+      print(df['A1Cresult'].unique())
+      # replace nan values with untested
+      df.replace({'max_glu_serum': {np.nan : "untested"}, 'A1Cresult': {np.nan: "untested"}}, inplace=True)
+      print(df['max_glu_serum'].unique())
+      print(df['A1Cresult'].unique())
+      # we are done dealing with the null values, We move on to check other columns and their values
+      print(df.info())
+      # we check the gender column as it's type is object and since it's categorical data we should
+      # change to to categories, same for races
+      print(df['gender'].unique())
+      df['gender'] = df['gender'].astype('category')
+      print(df['race'].value_counts())
+      # we notice that we got '?' among the categories so we replace it with 'Other' and change the 
+      # the type of the column to cateogry
+      df.replace({'race': {'?': 'Other'}}, inplace=True)
+      df['race'] = df['race'].astype('category')
+      # Now we move on to checking the uniqueness of both the visit id
+      print(df.shape[0])
+      print(df['encounter_id'].nunique())
+      # we move on to the admission_type_id to check it 
+      print(df['admission_type_id'].unique())
+      print(df[df['admission_type_id'] == 8]['diag_1'].head(10))
+      # we discover an important fact and it's as the admission type is ordered by ranking from 
+      # least dangerous eg. rank : '1' to life threating admission eg. rank 8 based on the diag_1
+      # col where the ICD-9 codes refer to the medical diagonsis based on a code system 
+      # on checking this values we find that admission_id with high ranks such 8 is diagnosed with 
+      # life-threating diseases so we map the values from 1 to 8 as the following:
+      admissionTypes = {1: 'referral from clinic', 2: 'pyschiatric emergency', 3: 'minor care',
+                         4: 'non-urgent', 5: 'semi-urgent', 6:'urgent', 7: 'emergent', 8: 'resuscitation'}
+      df['admission_type_id'] = df['admission_type_id'].map(admissionTypes).astype('category')
+      print(df['admission_type_id'].unique())
+      # We shall now check the age, weight columns
+      print(df['age'].unique())
+      print(df['weight'].unique())
+      # now we replace the '?' from the weights with not-recorded as it's more informative and we change
+      # both types to categorical
+      df.replace({'weight': {'?': 'not-recorded'}}, inplace=True)
+      df['weight'] = df['weight'].astype('category')
+      df['age'] = df['age'].astype('category')
+      # now we drop all the medicine-related cols 
+      medication_columns = ['metformin', 'repaglinide', 'nateglinide', 'chlorpropamide', 'glimepiride',
+                        'acetohexamide', 'glipizide', 'glyburide', 'tolbutamide', 'pioglitazone',
+                        'rosiglitazone', 'acarbose', 'miglitol', 'troglitazone', 'tolazamide',
+                        'examide', 'citoglipton', 'glyburide-metformin', 'glipizide-metformin',
+                        'glimepiride-pioglitazone', 'metformin-rosiglitazone', 'metformin-pioglitazone']
+      
+      df.drop(columns=medication_columns, inplace=True)
+      print(df.info())
+      
+
+
+
+
+
+
+
+
+      
+      
+
+
